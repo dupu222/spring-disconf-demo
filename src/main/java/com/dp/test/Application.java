5f0f392d24c243563a701271549dec7404fe3f92
dupu <dupu222@gmail.com>
2017-05-24 08:52:07 +0800

    feat : 项目搭建

---
+++ src/main/java/com/dp/test/Application.java
@@ -0,0 +1,14 @@
+package com.dp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dp on 2017/5/23.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
